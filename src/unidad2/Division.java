a98e55443a64bf4cbce87b0e847d6e0a58ef8392
raydi <raydi@raydiens>
2021-06-02 13:49:39 +0200

    Ejercicio n1 U2 resuelto

---
+++ src/unidad2/Division.java
@@ -0,0 +1,20 @@
+package unidad2;
+
+public class Division {
+
+	public static void main(String[] args) {
+		
+		
+		//
+		
+		
+		double valorA=1234;
+		
+		double valorB=532;
+		
+		System.out.println((valorA/valorB)*10);
+		
+
+	}
+
+}
